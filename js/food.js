680050b55d076618ea233bb8b9b420e0b85af98b
Shaun <seyyes2000@hotmail.com>
2016-01-21 21:15:25 -0500

    Add code to food.js

--- js/food.js
+++ js/food.js
@@ -1,7 +1,25 @@
-var Food = Backbone.Collection.extend({
-	url: '/food'
+$(function() {
+
+	// Create a model for the foods
+	var Food = Backbone.Collection.extend({
+		//url: '/food'
+		defaults: {
+			foodName: "Banana",
+			calories: 0
+		}
+	});
+
+	// Create a collection of foods
+	var FoodList = Backbone.Collection.extend({
+
+		// Hold objects of the food model
+		model: Food
+	});
+
+	// Prefil the collection with some foods.
+	var foods = new FoodList([
+		new Food({ foodName: 'Almond', calories: 100}),
+		new Food({ foodName: 'Orange', calories: 150})
+	])
+
 });
-
-
-
-
