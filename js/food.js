f73d8d8713c866fb00bca0ef996ecd99f5ac23d8
Shaun <seyyes2000@hotmail.com>
2016-01-22 23:15:52 -0500

    add code to food.js

--- js/food.js
+++ js/food.js
@@ -16,10 +16,33 @@ $(function() {
 		model: Food
 	});
 
-	// Prefil the collection with some foods.
+	// Prefill the collection with some foods.
 	var foods = new FoodList([
 		new Food({ foodName: 'Almond', calories: 100}),
 		new Food({ foodName: 'Orange', calories: 150})
-	])
+	]);
+
+	// This view turns the Food model into HTML.  Will create LI elements.
+	var FoodView = Backbone.View.extend({
+		tagName: 'Li',
+		events: {
+			'click': 'toggleService'
+		},
+
+		initialize: function(){
+			// Set up event listeners. The change backbone event
+			// is raised when a property changes (like the checked field)
+			this.listenTo(this.model, 'change', this.render);
+		},
+
+		render: function(){
+			// Create the HTML
+			this.$el.html()
+			this.$('input').prop()
+			// Returning the object is good practice tht makes chaining possible
+			return this;
+		}
+	})
+
 
 });
